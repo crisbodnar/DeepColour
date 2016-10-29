2c4d2299d74e62dd8be0b97582717ada0b7a18db
Malina <floreamalina@gmail.com>
2016-10-29 22:48:55 +0100

    server finally works

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = './uploads'
+UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg'])
 
 app = Flask(__name__)
@@ -13,11 +13,12 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
 	return render_template('index.html')
 
-def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['GET', 'POST'])
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
+
+
     if request.method == 'POST':
         if 'file' not in request.files:
             print 'No file part'
@@ -26,11 +27,12 @@ def upload_file():
         if file.filename == '':
             print 'No selected file'
             return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-    return 'ok'
+
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        return redirect('/')
+    else:
+        print 'elaw'
+        return 'ok'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
