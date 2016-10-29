94ed91855547fe57ea35f4c9182681bee4423eeb
Malina <floreamalina@gmail.com>
2016-10-29 21:27:34 +0100

    server v2

--- server.py
+++ server.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print 'No file part'
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            print 'No selected file'
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return 'ok'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
