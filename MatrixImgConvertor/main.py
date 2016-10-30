1e2ff78a88640504143d2c1e746a34db54087104
AndrewBLC <andreibelcin17aug@gmail.com>
2016-10-30 11:50:29 +0000

    Update main.py

--- MatrixImgConvertor/main.py
+++ MatrixImgConvertor/main.py
@@ -3,7 +3,7 @@ from random import randint
 #from PIL import Image
 
 matrix = []
-f = open("target2.txt", "r")
+f = open("color2d2.txt", "r")
 
 lines = f.readlines()
 matrix = [[int(number) for number in line.split()] for line in lines]
