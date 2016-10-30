e3493b6c0311d5923471d93f95ad63b63c5b2544
Malina <floreamalina@gmail.com>
2016-10-30 02:00:59 +0000

    html+css

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'uploads/'
@@ -13,12 +13,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def index():
 	return render_template('index.html')
 
-
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
-
-
     if request.method == 'POST':
         if 'file' not in request.files:
             print 'No file part'
@@ -29,10 +25,12 @@ def upload_file():
             return redirect(request.url)
 
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        return redirect('/')
-    else:
-        print 'elaw'
-        return 'ok'
+        return render_template('coloured_image.html', image_name = file.filename)
+        #return redirect(url_for('uploaded_file', filename=file.filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename);
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
